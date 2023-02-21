2bc1c42c08625c21c8fbc826554bab26eb729a97
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-21 21:06:12 +0000

    changed hardhat.config and deploy.js to deploy the contract to goerli and mumbai networks

--- Week2/contracts/MetanaNFT.sol
+++ Week2/contracts/MetanaNFT.sol
@@ -1,3 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MetanaNFT is ERC721 {
+    uint public totalSupply;
+    uint public constant MAX_SUPPLY = 10;
+
+    constructor() ERC721("Metana Collection", "MTN"){
+
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmSzfiayDizzpydoFN9SPFgP2MaGCkS85d1JLECy14DQUn/";
+    }
+
+    function mint() external {
+        require(totalSupply<MAX_SUPPLY);
+        _mint(msg.sender, totalSupply);
+        totalSupply++;
+    }
 
 }
