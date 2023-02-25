84e65437c317a2e8745390dab5a5741ace8eebd2
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-25 21:10:00 +0000

    tested staking with block.timestamp of 10 seconds

---
+++ Week4/contracts/Staking.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Staking{
+
+    uint public constant DURATION = 10 seconds;
+
+    struct Account{
+        uint amountStaked;
+        uint timeWhenStaked;
+    }
+
+    mapping(address => Account) public staked;
+
+    function deposit() external payable{
+        require(msg.value>0, "Staking: amount can't be 0");
+        staked[msg.sender].amountStaked += msg.value;
+        staked[msg.sender].timeWhenStaked = block.timestamp;
+
+    }
+
+    function withdraw() external returns(bool){
+        require(staked[msg.sender].amountStaked > 0, "Staking: You don't have funds in this contract");
+        require(block.timestamp - staked[msg.sender].timeWhenStaked > DURATION, "Staking: Please wait at least 10 seconds to withdraw");
+        uint balanceUser = staked[msg.sender].amountStaked;
+        (bool success, ) = payable(msg.sender).call{value: balanceUser }("");
+        staked[msg.sender].amountStaked -= balanceUser;
+        return success;
+    }
+
+}
