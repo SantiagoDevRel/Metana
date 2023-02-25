b4c4c9d25f22e1b1857b1419f8e07dda55f5fe9d
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-25 12:10:17 +0000

    testing bigNumbers contract

---
+++ Week4/contracts/BigNumbers.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//import "hardhat/console.sol";
+
+contract BigNumber {
+
+    uint256 public number;
+
+    constructor (uint _number) {
+        number = _number;
+    }
+
+    function setToTheMax() external{
+        number = type(uint256).max;
+        //console.log(number);
+    }
+
+    function getNumber() external view returns(uint256){
+        return number;
+    }
+
+}
---
+++ Week4/test/BigNumbers.js
@@ -0,0 +1,35 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat")
+const { BigNumber } = ethers;
+
+describe("BigNumbers Contract", function () {
+
+  let contract = null;
+  beforeEach(async () => {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Contract = await ethers.getContractFactory("BigNumber");
+    contract = await Contract.deploy(0);
+    await contract.deployed()
+  })
+
+  it("Check if number is 0", async () => {
+    expect(await contract.getNumber()).to.be.equal(0)
+  })
+
+  it("Set to the max", async () => {
+    const tx = await contract.setToTheMax()
+    await tx.wait()
+
+    expect(await contract.getNumber()).to.be.equal(
+      new BigNumber.from("115792089237316195423570985008687907853269984665640564039457584007913129639935")
+    )
+  })
+
+
+})
