dfcb3ade06497aa287f3e4368dc6b43e48633ae7
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-25 19:02:00 +0000

    testing events

---
+++ Week4/test/Events.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat")
+const { BigNumber } = ethers;
+
+
+describe("Test Events contract", () => {
+    let contract = null, deployerSigner = null, user2Signer = null;
+
+    beforeEach(async () => {
+        const [deployer, user2] = await ethers.getSigners()
+        deployerSigner = deployer
+        user2Signer = user2
+        const Contract = await ethers.getContractFactory("Events");
+        contract = await Contract.deploy();
+        await contract.deployed()
+    })
+
+
+    it("Should emit first event", async () => {
+        //Test if the function emits and events with specific arguments
+        expect(await contract.connect(deployerSigner).firstEvent()).to.emit(contract, "First").withArgs(deployerSigner.address)
+    })
+
+    it("Should emit second event", async () => {
+        expect(await contract.connect(user2Signer).secondEvent()).to.emit(contract, "Second").withArgs()
+
+    })
+
+
+
+
+
+
+
+
+})
