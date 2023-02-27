f0513945f3631c6422a20330397935925c841092
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-27 10:58:15 +0000

    PartialRefund.sol tested 100% everything

--- Week1/contracts/PartialRefund.sol
+++ Week1/contracts/PartialRefund.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PartialRefund is ERC20 {
     address public owner;
+    uint public constant MAX_SUPPLY = 1_000_000;
   
     constructor() ERC20("PartialRefund","METANA"){
         owner = msg.sender;
@@ -13,29 +14,29 @@ contract PartialRefund is ERC20 {
     //Function to mint, paying exactly 1 ether
     function preSale() external payable {
         require(msg.value == 1 ether, "ERC20: Pay only 1 ether");
-        require(totalSupply() <= 1000000*10**decimals(), "ERC20: MaxSupply reached");
+        require(totalSupply() < MAX_SUPPLY*10**decimals(), "ERC20: MaxSupply reached");
         _mint(msg.sender, 1000*10**decimals());
     }
 
     //Function to withdraw the funds to the owner, only the owner can call it
     function withdrawFunds() external returns (bool){
-        require(msg.sender == owner);
+        require(msg.sender == owner, "ERC20: You are not the owner");
         (bool success, ) = payable(owner).call{value: address(this).balance}("");
         return success;
     }
 
     /*
         * sellBack() --> Users can transfer their tokens to the contract and receive 0.5 ether for every 1000 tokens they transfer. 
-        * 1. User must approve tokens first
+        * 1. transfer tokens from user to this contract
         * 2. calculate amount to send to the user
         * 2.1 for every token the user is sending to this contract, will claim 0.0005 ether (or 500000000000000 wei)
         * 3. revertTx() is address(this )contract doesn't have enough balance
         * 4. send ether to the user and return success
     */
     function sellBack(uint amount) public returns(bool) {
-        transferFrom(msg.sender, address(this), amount*10**decimals()); 
-        uint amountToGiveBack = (amount*10**decimals()) * 0.0005 ether;
-        require(address(this).balance >= amountToGiveBack, "Not enough ether in this contract");
+        transfer(address(this), amount*10**decimals()); 
+        uint amountToGiveBack = (amount) * 0.0005 ether;
+        require(address(this).balance >= amountToGiveBack, "ERC20: Not enough ether in this contract");
         (bool success, ) = payable(msg.sender).call{value: amountToGiveBack}("");
         return success;
     }
--- Week1/test/partialrefund.test.js
+++ Week1/test/partialrefund.test.js
@@ -20,10 +20,10 @@ describe("Test PartialRefund contract", () => {
     contract = await Contract.deploy();
     await contract.deployed();
 
-    //Send 10 ether to user2
+    //Send 2000 ether to user2 & 1 ether to owner;
     provider = ethers.provider;
     const amountToMint = utils.hexStripZeros(
-      utils.parseEther("1001").toHexString()
+      utils.parseEther("2000").toHexString()
     );
     const balanceOfOwner = utils.hexStripZeros(
       utils.parseEther("1").toHexString()
@@ -41,10 +41,10 @@ describe("Test PartialRefund contract", () => {
     console.log(utils.formatEther(balanceOfOwner));
   });
 
-  it("User2 balance should be 1001 ethers", async () => {
+  it("User2 balance should be 2000 ethers", async () => {
     const balanceOfUser2 = await provider.getBalance(user2.address);
     expect(balanceOfUser2).to.be.equal(
-      new BigNumber.from(utils.parseEther("1001"))
+      new BigNumber.from(utils.parseEther("2000"))
     );
   });
 
@@ -55,16 +55,159 @@ describe("Test PartialRefund contract", () => {
     );
   });
 
-  it("PreSale() should fail --> pay 2 ethers", async () => {
+  it("PreSale() paying 2 ethers - reverted", async () => {
     await expect(
       contract.connect(user2).preSale({ value: utils.parseEther("2") })
     ).to.be.revertedWith("ERC20: Pay only 1 ether");
   });
-  /* 
-  it("PreSale() should fail --> maxSupply reached", async () => {
-    for(let i=0;i<1000;i++){
-      const tx = await contract.
 
+  it("PreSale() should fail --> maxSupply reached", async () => {
+    const oneEther = utils.parseEther("1");
+    const preSale = async () => {
+      const tx = await contract.connect(user2).preSale({ value: oneEther });
+      await tx.wait();
+    };
+    //Mint x1001 times
+    for (let i = 0; i <= 1000; i++) {
+      if (i === 1000) {
+        await expect(preSale()).to.be.revertedWith("ERC20: MaxSupply reached");
+        break;
+      }
+      await preSale();
     }
-  }); */
+
+    //expect balance of the contract to be 1_000 ethers
+    expect(await provider.getBalance(contract.address)).to.be.equal(
+      new BigNumber.from(utils.parseEther("1000"))
+    );
+  });
+
+  it("PreSale() x1 time by user2", async () => {
+    const oneEther = utils.parseEther("1");
+    const tx = await contract.connect(user2).preSale({ value: oneEther });
+    await tx.wait();
+
+    const balanceOfUser = await provider.getBalance(user2.address);
+    const newBalanceOfUser = utils.parseEther("1999");
+    expect(balanceOfUser).to.be.closeTo(
+      new BigNumber.from(newBalanceOfUser),
+      new BigNumber.from(utils.parseEther("0.0001"))
+    );
+  });
+
+  it("withdrawFunds() by the owner", async () => {
+    //PreSale() 1 ether by user2
+    const oneEther = utils.parseEther("1");
+    const tx = await contract.connect(user2).preSale({ value: oneEther });
+    await tx.wait();
+
+    //Withdraw funds (1 ether) by owner
+    const withdraw = await contract.connect(owner).withdrawFunds();
+    await withdraw.wait();
+
+    //new balance of owner should be 2 ether - gasFees
+    const newBalanceOfOwner = await provider.getBalance(owner.address);
+    expect(newBalanceOfOwner).to.be.closeTo(
+      new BigNumber.from(utils.parseEther("2")),
+      new BigNumber.from(utils.parseEther("0.0001"))
+    );
+  });
+
+  it("withdrawFunds() by the user2 - reverted", async () => {
+    //PreSale() 1 ether by user2
+    const oneEther = utils.parseEther("1");
+    const tx = await contract.connect(user2).preSale({ value: oneEther });
+    await tx.wait();
+
+    //Withdraw funds (1 ether) by user2
+    await expect(contract.connect(user2).withdrawFunds()).to.be.revertedWith(
+      "ERC20: You are not the owner"
+    );
+  });
+
+  it("Test withdrawFunds() with callStatic", async () => {
+    //test with Owner
+    const txByOwner = await contract.connect(owner).callStatic.withdrawFunds();
+    expect(txByOwner).to.be.equal(true);
+
+    //test with User2
+    await expect(
+      contract.connect(user2).callStatic.withdrawFunds()
+    ).to.be.revertedWith("ERC20: You are not the owner");
+  });
+
+  it("SellBack() by the User2", async () => {
+    //Mint (preSal()) 10000 tokens by user2
+    const oneEther = utils.parseEther("1");
+    const tx = await contract.connect(user2).preSale({ value: oneEther });
+    await tx.wait();
+
+    //user should own 1000 tokens
+    const oneThousandTokens = utils.parseEther("1000");
+    expect(await contract.balanceOf(user2.address)).to.be.equal(
+      oneThousandTokens
+    );
+
+    /*  //approve 1000 tokens from user to the contract
+    const approve = await contract
+      .connect(user2)
+      .approve(contract.address, oneThousandTokens);
+    await approve.wait(); 
+
+    //check allowance from user by the contract = 1000 tokens
+    const allowanceFromContractByUser = await contract.allowance(
+      user2.address,
+      contract.address
+    );
+    expect(allowanceFromContractByUser).to.be.equal(oneThousandTokens);
+    */
+
+    //test sellBack() by user2
+    const sellBack = await contract.connect(user2).sellBack(1000);
+    await sellBack.wait();
+
+    //user2 should have 0 tokens
+    const newBalanceOfUser = await contract.balanceOf(user2.address);
+    expect(newBalanceOfUser).to.be.equal(utils.parseEther("0"));
+  });
+
+  it("SellBack() fails because no funds in the contract", async () => {
+    //Mint (preSal()) 10000 tokens by user2
+    const oneEther = utils.parseEther("1");
+    const tx = await contract.connect(user2).preSale({ value: oneEther });
+    await tx.wait();
+
+    //user should own 1000 tokens
+    const oneThousandTokens = utils.parseEther("1000");
+    expect(await contract.balanceOf(user2.address)).to.be.equal(
+      oneThousandTokens
+    );
+
+    /* //approve 1000 tokens from user to the contract
+    const approve = await contract
+      .connect(user2)
+      .approve(contract.address, oneThousandTokens);
+    await approve.wait(); 
+
+    //check allowance from user by the contract = 1000 tokens
+    const allowanceFromContractByUser = await contract.allowance(
+      user2.address,
+      contract.address
+    );
+    expect(allowanceFromContractByUser).to.be.equal(oneThousandTokens);
+      */
+
+    //withdraw contract funds
+    const withdraw = await contract.connect(owner).withdrawFunds();
+    await withdraw.wait();
+
+    //test sellBack() by user2
+    await expect(contract.connect(user2).sellBack(1000)).to.be.revertedWith(
+      "ERC20: Not enough ether in this contract"
+    );
+
+    //user2 should have 1000 tokens
+    const newBalanceOfUser = await contract.balanceOf(user2.address);
+    expect(newBalanceOfUser).to.be.equal(utils.parseEther("1000"));
+  });
 });
