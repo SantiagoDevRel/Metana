4f5972fb3d89ef6e7649b9d4d045e3c9df958f5b
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-27 08:53:35 +0000

    fixed decimals in partialRefund.sol + created hardhat project

--- Week1/contracts/PartialRefund.sol
+++ Week1/contracts/PartialRefund.sol
@@ -13,7 +13,7 @@ contract TokenSaleWithSellBack is ERC20 {
     //Function to mint, paying exactly 1 ether
     function preSale() external payable {
         require(msg.value == 1 ether && totalSupply() <= 1000000);
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000*10**decimals());
     }
 
     //Function to withdraw the funds to the owner, only the owner can call it
@@ -23,20 +23,21 @@ contract TokenSaleWithSellBack is ERC20 {
         return success;
     }
 
-    //Users can transfer their tokens to the contract and receive 0.5 ether for every 1000 tokens they transfer. 
+    /*
+        * sellBack() --> Users can transfer their tokens to the contract and receive 0.5 ether for every 1000 tokens they transfer. 
+        * 1. User must approve tokens first
+        * 2. calculate amount to send to the user
+        * 2.1 for every token the user is sending to this contract, will claim 0.0005 ether (or 500000000000000 wei)
+        * 3. revertTx() is address(this )contract doesn't have enough balance
+        * 4. send ether to the user and return success
+    */
     function sellBack(uint amount) public returns(bool) {
-        //approve this smart contract to withdraw funds from the user's balance
-        super.approve(address(this), amount);
+        transferFrom(msg.sender, address(this), amount*10**decimals()); 
+        
+        uint amountToGiveBack = (amount*10**decimals()) * 0.0005 ether;
 
-        //super.transfer(address(this), amount); //MAYBE I should transfer the tokens to address(this) contract instead of approving?
-         
-        //for every TokenSale that the user is giving allowance, send back to the user 0.0005 ether (or 500000000000000 wei)
-        uint amountToGiveBack = amount * 0.0005 ether;
-
-        //revert TX if the contract doesn't have enough ether
         require(address(this).balance >= amountToGiveBack, "Not enough ether in this contract");
 
-        //send ether to the user
         (bool success, ) = payable(msg.sender).call{value: amountToGiveBack}("");
 
         return success;
