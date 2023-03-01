f1717b4fd180aadfa8236a28988163fc9c50c4c9
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-03-01 19:41:21 +0000

    created erc20logs

--- Week5/eth-client-assignment/src/ERC20Logs.js
+++ Week5/eth-client-assignment/src/ERC20Logs.js
@@ -1,7 +1,164 @@
-import React from "react";
+import React, { useState } from "react";
+import { Network, Alchemy } from "alchemy-sdk";
+import { ethers } from "ethers";
+import styles from "./ERC20Logs.module.css";
+
+const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const TransferSignature =
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+const ApproveSignature =
+  "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
 
 function ERC20Logs() {
-  return <div>ERC20</div>;
+  let MAX_REQUESTS = 10,
+    currentRequests = 0;
+  const [transactionsArray, setTransactionsArray] = useState([]);
+  const [approvalsArray, setApprovalsArray] = useState([]);
+
+  const settings = {
+    apiKey: "uk3L_f3i_POcVTRhWxKrYlUm__ftnzfm",
+    network: Network.ETH_MAINNET,
+  };
+
+  const alchemy = new Alchemy(settings);
+
+  async function getLast10Block() {
+    const latestBlock = await alchemy.core.getBlockNumber();
+    return latestBlock - 10;
+  }
+
+  const fetchLogs = async (logSignature) => {
+    //get last 10 blocks
+    const last10Block = await getLast10Block();
+    //fetch the logs the last 10 blocks
+    const Logs = await fetch(
+      "https://eth-mainnet.g.alchemy.com/v2/uk3L_f3i_POcVTRhWxKrYlUm__ftnzfm",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          id: 0,
+          method: "eth_getLogs",
+          params: [
+            {
+              fromBlock: `${ethers.utils.hexlify(last10Block)}`,
+              address: DAIAddress,
+              topics: [`${logSignature}`],
+            },
+          ],
+        }),
+      }
+    );
+    return await Logs.json();
+  };
+
+  const formatLogs = async (typeOfLog) => {
+    const auxArr = [];
+    const logsState = await fetchLogs(typeOfLog);
+    const result = logsState.result;
+    if (typeOfLog === `${TransferSignature}`) {
+      //format events Transfer()
+      for (let i = 0; i < result.length; i++) {
+        const _txHash = `https://etherscan.io/tx/${result[i]["transactionHash"]}`;
+        const _from = ethers.utils.hexStripZeros(result[i]["topics"][1]);
+        const _to = ethers.utils.hexStripZeros(result[i]["topics"][2]);
+        const tx = { id: i, txHash: _txHash, from: _from, to: _to };
+        //console.log("TRANSACTIONS", i, tx);
+        //update state
+        if (auxArr.length === 5) {
+          auxArr.shift();
+          auxArr.push(tx);
+        } else {
+          auxArr.push(tx);
+        }
+      }
+      setTransactionsArray(auxArr);
+    } else {
+      //format events Approval()
+      for (let i = 0; i < result.length; i++) {
+        const _txHash = `https://etherscan.io/tx/${result[i]["transactionHash"]}`;
+        const _from = ethers.utils.hexStripZeros(result[i]["topics"][1]);
+        const _to = ethers.utils.hexStripZeros(result[i]["topics"][2]);
+        const tx = { id: i, txHash: _txHash, from: _from, to: _to };
+        //update state
+        if (auxArr.length === 5) {
+          auxArr.shift();
+          auxArr.push(tx);
+        } else {
+          auxArr.push(tx);
+        }
+      }
+      setApprovalsArray(auxArr);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.containerButtons}>
+        <button
+          className={styles.button}
+          onClick={() => formatLogs(`${TransferSignature}`)}
+        >
+          Get Transfer Logs
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => formatLogs(`${ApproveSignature}`)}
+        >
+          Get Approval Logs
+        </button>
+      </div>
+      <div className={styles.containerTxs}>
+        {transactionsArray.length >= 1 ? (
+          <ol className={styles.logs}>
+            {transactionsArray.map((tx) => (
+              <li className={styles.everyLog} key={tx.id}>
+                <div>From: {tx.from}</div>
+                <div>To: {tx.to}</div>
+                <div>
+                  <a href={tx.txHash} target="_blank" rel="noreferrer">
+                    View in Etherscan{" "}
+                    <img
+                      className={styles.img}
+                      alt="Etherscan Logo"
+                      src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg"
+                    />
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <div>0 Transfers in the past 10 blocks.</div>
+        )}
+        {approvalsArray.length >= 1 ? (
+          <ol className={styles.logs}>
+            {approvalsArray.map((tx) => (
+              <li className={styles.everyLog} key={tx.id}>
+                <div>Approval From: {tx.from}</div>
+                <div>Spender: {tx.to}</div>
+                <div>
+                  <a href={tx.txHash} target="_blank" rel="noreferrer">
+                    View in Etherscan{" "}
+                    <img
+                      className={styles.img}
+                      alt="Etherscan Logo"
+                      src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg"
+                    />
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <div>0 Approval in the past 10 blocks.</div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default ERC20Logs;
