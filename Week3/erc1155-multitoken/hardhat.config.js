23ffb491d3635d0a7aee35dda04afb4ab8a19a8d
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-03-12 19:56:07 +0000

    Updated

--- Week3/erc1155-multitoken/hardhat.config.js
+++ Week3/erc1155-multitoken/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   solidity: "0.8.9",
   defaultNetwork: "hardhat",
   paths: {
-    artifacts: "./src/artifacts",
+    /* artifacts: "./src/artifacts", */
   },
   /*  networks: {
     goerli: {
