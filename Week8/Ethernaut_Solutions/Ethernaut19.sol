4bd99dea1ded07d32aaa9d2faafc4f57aab38023
your_username <stztrujillo@hotmail.com>
2023-03-24 07:47:13 +0000

    Solved Ethernaut19

--- Week8/Ethernaut_Solutions/Ethernaut19.sol
+++ Week8/Ethernaut_Solutions/Ethernaut19.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.5.0;
     slot 0 - 21bytes --> address(owner) 20 bytes + bool contact;  
     slot 1 - 32bytes --> bytes32[] codex.length;
 
-    1. we will create an underflow in bytes[] codex calling retract to do codex.length-- (2**256-1)
-    2. we will find the storage location of the owner by doing (2**256-keccak(slot1))
+    1. We will create an underflow in bytes[] codex calling retract to do codex.length-- (2**256-1)
+    2. We will find the storage location of the owner by doing (2**256-keccak(slot1))
     3. using the function revise(uint i, bytes _content) we will be able to set the slot for the new owner
 */
 
