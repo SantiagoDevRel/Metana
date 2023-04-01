9bc72d168ccd385329d95544ffdae95afe32265f
your_username <stztrujillo@hotmail.com>
2023-04-01 09:49:26 +0100

    added merkleProof

--- Week9/AdvanceNFT/contracts/AdvancedNFT.sol
+++ Week9/AdvanceNFT/contracts/AdvancedNFT.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract AdvancedNFT is ERC721, Ownable, ReentrancyGuard {
-
     //~~~~~~~ Libraries ~~~~~~~
     //Import the BitMap library and point it to the data type BitMaps.BitMap struct
     using BitMaps for BitMaps.BitMap;
@@ -16,75 +16,53 @@ contract AdvancedNFT is ERC721, Ownable, ReentrancyGuard {
     event RevealedRandomTokenId(uint256 _randomNumber, address _owner);
 
     //~~~~~~~ Structs ~~~~~~~
-    struct Commit{
+    struct Commit {
         bytes32 commitedHash;
         uint256 blockNumber;
         uint256 tokenIdForNFT;
     }
 
     //~~~~~~~ Enum ~~~~~~~
-    enum States{
+    enum States {
         CLOSED,
-        PRESALE,
-        PUBLICSALE,
-        SOLDOUT,
-        CLAIM_NFT
+        MINT_PRIVATE_LIST,
+        MINT_PUBLIC_LIST
     }
 
     //~~~~~~~ State variables ~~~~~~~
-    uint256 constant MAX_SUPPLY = 99;
-    uint256 constant PRIVATE_MINT_SUPPLY = 9;
+    uint256 public immutable MAX_SUPPLY;
+    uint256 public immutable PRIVATE_MINT_SUPPLY;
+    bytes32 public immutable PRIVATE_LIST_MERKLE_ROOT;
+    bytes32 public immutable PUBLIC_LIST_MERKLE_ROOT;
     uint256 public s_totalSupply;
-    States public s_state = States.CLOSED;
+    States public s_state;
     BitMaps.BitMap private s_myBitMap;
-    address[] public s_teamMembersArr;
-    mapping (address => bool) s_userMinted;
-    mapping (address => Commit) public s_commits;
-    mapping (address => bool) public s_teamMembersMap;
-
-
+    mapping(address => bool) s_userMinted;
+    mapping(address => Commit) public s_commits;
 
     //~~~~~~~ Constructor ~~~~~~~
-    constructor() ERC721("NFBIT", "NFB"){
-        for(uint i=0;i<99;++i){
-            s_myBitMap.set(i);
-        }
-        s_teamMembersMap[msg.sender] = true;
-        s_teamMembersArr.push(msg.sender);
+    constructor(
+        uint256 _privateSupply,
+        uint256 _maxSupply,
+        bytes32 _privateRoot,
+        bytes32 _publicRoot
+    ) ERC721("NFBIT", "NFB") {
+        s_state = States.CLOSED;
+        MAX_SUPPLY = _maxSupply;
+        PRIVATE_MINT_SUPPLY = _privateSupply;
+        PRIVATE_LIST_MERKLE_ROOT = _privateRoot;
+        PUBLIC_LIST_MERKLE_ROOT = _publicRoot;
     }
 
     //~~~~~~~ onlyAdmin/Team Functions ~~~~~~~
-    
-    //Activate the preSale state 
-    function openPrivateSale() external onlyOwner{
-        require(s_state == States.CLOSED, "NFB: Current state must be CLOSED");
-        s_state = States.PRESALE;
+
+    //Activate the preSale state
+    function openPrivateMint() external onlyOwner {
+        s_state = States.MINT_PRIVATE_LIST;
     }
 
-    //Activate the ClaimNFB state so the users can claim their NFT
-    function activateClaim() external onlyOwner{
-        require(s_state == States.SOLDOUT, "NFB: Current state must be SOLDOUT");
-        s_state = States.CLAIM_NFT;
-    }
-
-    //Add a team member
-    function addMember(address _member) external onlyOwner{
-        s_teamMembersMap[_member] = true;
-        s_teamMembersArr.push(_member);
-    }
-
-    //Function for the team to withdraw funds equally
-    function withdrawFunds() external nonReentrant {
-        require(s_teamMembersMap[msg.sender], "NFB: You are not a team member");
-        address[] memory _teamMembers = s_teamMembersArr;
-        uint256 _amount = address(this).balance / _teamMembers.length;
-        for(uint i=0;i<_teamMembers.length;){
-            (bool success, ) = payable(_teamMembers[i]).call{value: _amount}("");
-            require(success, "NFB: Failed transfering funds to the team");
-            unchecked{
-                ++i;
-            }
-        }
+    function openPublicMint() external onlyOwner {
+        s_state = States.MINT_PUBLIC_LIST;
     }
 
     //~~~~~~~ External / Public Functions ~~~~~~~
@@ -98,99 +76,143 @@ contract AdvancedNFT is ERC721, Ownable, ReentrancyGuard {
     }
 
     //2nd step of minting/buying --> getYourTokenId() by verifying your commit
-    function getYourTokenId(uint _tokenId, uint _salt) internal {
+    function getYourTokenId(uint256 _randomUserNumber, uint256 _salt) internal {
         Commit memory _commit = s_commits[msg.sender];
-        require(_commit.tokenIdForNFT == 0,"NFB: Random token ID was already revealed");
-        require(block.number < _commit.blockNumber + 255, "NFB: Too late, please submit your commit again");
-        require(block.number > _commit.blockNumber + 10, "NFB: Please verify after 10 blocks.");
-        require(getYourHash(_tokenId,_salt) == _commit.commitedHash , "NFB: Wrong hash");
+        require(
+            _commit.tokenIdForNFT == 0,
+            "NFB: Random token ID was already revealed"
+        );
+        require(
+            block.number < _commit.blockNumber + 255,
+            "NFB: Too late, please submit your commit again"
+        );
+        require(
+            block.number > _commit.blockNumber + 10,
+            "NFB: Please verify after 10 blocks."
+        );
+        require(
+            getYourHash(_randomUserNumber, _salt) == _commit.commitedHash,
+            "NFB: Wrong hash"
+        );
         bytes32 commitedBlockHash = blockhash(_commit.blockNumber);
         uint256 MAX_RANDOM_NUMBER_FOR_TOKEN_ID = 1000000000000;
-        bytes32 randomHash = bytes32(keccak256(abi.encodePacked(commitedBlockHash,_commit.commitedHash)));
-        uint256 randomNumber = uint256(randomHash)%MAX_RANDOM_NUMBER_FOR_TOKEN_ID;
-        s_commits[msg.sender].tokenIdForNFT = randomNumber;  
+        bytes32 randomHash = bytes32(
+            keccak256(abi.encodePacked(commitedBlockHash, _commit.commitedHash))
+        );
+        uint256 randomNumber = uint256(randomHash) %
+            MAX_RANDOM_NUMBER_FOR_TOKEN_ID;
+        s_commits[msg.sender].tokenIdForNFT = randomNumber;
         emit RevealedRandomTokenId(randomNumber, msg.sender);
     }
 
     //3rd step of minting/buying --> mint()
 
-    //only for users registered in the private round
-    function privateRoundMint() external {
-        require(s_state == States.PRESALE, "NFB: Minting is not in pre sale.");
-        //1. update the ticket of the address to 0
-        //2. check if the address is registered
+    //Only users registered in the early private round can mint here
+    function privateRoundMint(uint256 _ticketNumber, bytes32[] memory _proof)
+        external
+    {
+        require(
+            s_state == States.MINT_PRIVATE_LIST,
+            "NFB: Minting is not in pre sale."
+        );
+        require(
+            _userHasAValidPrivateTicket(_ticketNumber, _proof),
+            "NFB: Invalid ticket number"
+        );
+        require(
+            _verifyAndUseTicket(_ticketNumber),
+            "NFB: Ticket was already spent."
+        );
         _allocateToken();
     }
 
-    //anyone can call this one and mint by paying 0.001
-    function publicRoundMint() external payable{
-        require(s_state == States.PUBLICSALE, "NFB: Minting is not in public sale.");
-        require(msg.value == 0.001 ether, "NFB: Please pay 0.001 ether to mint.");
+    //Only users registered for the public sale can mint here
+    function publicRoundMint() external payable {
+        require(
+            s_state == States.MINT_PUBLIC_LIST,
+            "NFB: Minting is not in public sale."
+        );
+        require(
+            msg.value == 0.001 ether,
+            "NFB: Please pay 0.001 ether to mint."
+        );
         _allocateToken();
     }
 
     //~~~~~~~ Internal Functions ~~~~~~~
 
+    function _userHasAValidPrivateTicket(
+        uint256 _ticketNumber,
+        bytes32[] memory _proof
+    ) internal view returns (bool) {
+        bytes32 _root = PRIVATE_LIST_MERKLE_ROOT;
+        bytes32 _leaf = keccak256(abi.encodePacked(msg.sender, _ticketNumber));
+        return MerkleProof.verify(_proof, _root, _leaf);
+    }
+
+    function _verifyAndUseTicket(uint256 _ticketNumber)
+        internal
+        returns (bool)
+    {
+        require(!s_myBitMap.get(_ticketNumber), "NFB: Ticket already spent.");
+        s_myBitMap.set(_ticketNumber);
+        return true;
+    }
+
     function _allocateToken() internal {
+        s_totalSupply++;
         uint256 _totalSupply = s_totalSupply;
         uint256 _tokenId = s_commits[msg.sender].tokenIdForNFT;
         _safeMint(msg.sender, _tokenId);
         _updateState(_totalSupply);
-        s_totalSupply++;
     }
 
     function _updateState(uint256 _totalSupply) internal {
-        //Change state to PUBLISALE, once PRIVATE_MINT_SUPPLY has been minted.
-        if(_totalSupply == PRIVATE_MINT_SUPPLY){
-            s_state = States.PUBLICSALE;
-        
+        //Change state to PUBLIC_MINT, once PRIVATE_MINT_SUPPLY has been minted.
+        if (_totalSupply == PRIVATE_MINT_SUPPLY) {
+            s_state = States.MINT_PUBLIC_LIST;
         }
-        //Change state to SOLDOUT, once MAX_SUPPLY has been minted
-        else if(_totalSupply == MAX_SUPPLY){
-            s_state = States.SOLDOUT;
+        //Change state to CLOSED, once MAX_SUPPLY has been reached
+        else if (_totalSupply == MAX_SUPPLY) {
+            s_state = States.CLOSED;
         }
     }
 
     //~~~~~~~ Pure / View Functions ~~~~~~~
-    function getYourHash(uint256 _tokenId, uint256 _salt) public pure returns(bytes32){
-        return keccak256(abi.encodePacked(_tokenId, _salt));
+    function getYourHash(uint256 _randomUserNumber, uint256 _salt)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_randomUserNumber, _salt));
     }
 
     //Manage the datastructure bits
-    function getBit(uint256 _index) external view returns(bool){
+    function getBit(uint256 _index) external view returns (bool) {
         return s_myBitMap.get(_index);
     }
 
-    function setToBit(uint _index, bool _value) external {
+    function setToBit(uint256 _index, bool _value) external {
         s_myBitMap.setTo(_index, _value);
     }
 
-    function setBit(uint256 _index) external {//43971 tx cost - 22767 ex cost
+    function setBit(uint256 _index) external {
+        //43971 tx cost - 22767 ex cost
         s_myBitMap.set(_index);
     }
 
-    function unsetBit(uint256 _index) external { //30942 tx cost (from 1 to 0)
+    function unsetBit(uint256 _index) external {
+        //30942 tx cost (from 1 to 0)
         s_myBitMap.unset(_index);
     }
 
-    function setMapping() external {//43509 tx cost - 22445 ex cost
+    function setMapping() external {
+        //43509 tx cost - 22445 ex cost
         s_userMinted[msg.sender] = true;
-    }    
-    
-    function getMapping() external view returns(bool){
+    }
+
+    function getMapping() external view returns (bool) {
         return s_userMinted[msg.sender];
-    }
-
-
-
-    //~~~~~~~~~~~~~~~~~~~~~~~~~ remove ~~~~~~~~~~~~~~~~~~~~~~~~
-
-    function getblock() external view returns(uint256){
-        return block.number;
-    }
-
-    function getBlockHash() external view returns(bytes32){
-        return blockhash(s_commits[msg.sender].blockNumber);
     }
 
 }
