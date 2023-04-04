1e686a031c5da6a0475e27067181fd04f01089c7
your_username <stztrujillo@hotmail.com>
2023-04-04 08:22:40 +0100

    added initializable inheritance

--- Week10/Proxies/contracts/Box.sol
+++ Week10/Proxies/contracts/Box.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Box {
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+contract Box is Initializable {
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    address public immutable deployer;
     
     uint256 private value;
 
-    event updatedValue(uint256 newValue);
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(){
+        deployer = msg.sender;
+    }
+
+
+    function initialize (uint256 _newValue) external initializer() {
+        value = _newValue;
+    }
 
     function store(uint256 _newValue) public{
         value = _newValue;
-        emit updatedValue(_newValue);
     }
 
     function getValue() external view returns(uint256){
--- Week10/Proxies/contracts/BoxV2.sol
+++ Week10/Proxies/contracts/BoxV2.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+//import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
 contract BoxV2 {
     
     uint256 private value;
 
-    event updatedValue(uint256 newValue);
-
     function store(uint256 _newValue) public{
         value = _newValue;
-        emit updatedValue(_newValue);
     }
 
     function getValue() external view returns(uint256){
