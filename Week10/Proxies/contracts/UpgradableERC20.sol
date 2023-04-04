6d8968f517bea1035b2769ae3afd158ae91c237a
your_username <stztrujillo@hotmail.com>
2023-04-04 18:16:23 +0100

    Added version2 to upgrade the proxy

--- Week10/Proxies/contracts/UpgradableERC20.sol
+++ Week10/Proxies/contracts/UpgradableERC20.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MyTokenUpgradable is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+contract MyTokenUpgradable is Initializable, ERC20Upgradeable, OwnableUpgradeable,UUPSUpgradeable {
     //Upgradable can't have a constructor
     //constructor() ERC20("MyToken", "MTK") {}
     //Instead we will call the init() function for each inherited contract
@@ -17,5 +18,17 @@ contract MyTokenUpgradable is Initializable, ERC20Upgradeable, OwnableUpgradeabl
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {
+        
+    }
+
+}
+
+contract MyTokenUpgradableV2 is MyTokenUpgradable {
+
+    function version() external pure returns(string memory){
+        return "v2!";
     }
 }
