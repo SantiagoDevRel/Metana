ebf76e36caced6e4c3e53e14c53a42fb4a4856a2
your_username <stztrujillo@hotmail.com>
2023-04-07 12:37:27 +0100

    update everything into 1 script

--- Week10/Assignments/scripts/deploy3contracts.js
+++ Week10/Assignments/scripts/deploy3contracts.js
@@ -1,46 +1,45 @@
 const { ethers, upgrades } = require("hardhat");
 
-async function deployUpERC20() {
-  
+async function main() {
+  //Deploy ERC20
   const UpERC20 = await ethers.getContractFactory("UpERC20");
   
   const erc20 = await upgrades.deployProxy(UpERC20, ["MetaToken", "MTN"], {initializer: "init", kind: "uups"});
   
   await erc20.deployed();
-  let address = erc20.address;
-  console.log("erc20 deployed to:", address);
-  return address;
-}
-
-async function deployUpERC721() {
+  const ADDRESS_ERC20 = erc20.address;
+  console.log("erc20 deployed to:", ADDRESS_ERC20);
   
+  //Deploy ERC721
   const UpERC721 = await ethers.getContractFactory("UpNFT");
   
   const erc721 = await upgrades.deployProxy(UpERC721, ["MetaNFT", "MNFT"], {initializer: "init", kind: "uups"});
   
   await erc721.deployed();
   
-  let address = erc721.address
-  console.log("erc721 deployed to:", address);
-  return address;
-}
+  const ADDRESS_ERC721 = erc721.address
+  console.log("erc721 deployed to:", ADDRESS_ERC721);
 
-async function deployStakingUpgradeable() {
-  //deploy token & nft
-  const addressUpERC20 = await deployUpERC20()
-  const addressUpERC721 = await deployUpERC721()
-
+  //Deploy staking
   const UpStaking = await ethers.getContractFactory("UpStaking");
   
-  const upStaking = await upgrades.deployProxy(UpStaking, [addressUpERC20, addressUpERC721], {initializer: "init", kind: "uups"});
+  const upStaking = await upgrades.deployProxy(UpStaking, [ADDRESS_ERC20, ADDRESS_ERC721], {initializer: "init", kind: "uups"});
+
   
   await upStaking.deployed();
   
-  let address = upStaking.address
-  console.log("staking deployed to:", address);
+  const ADDRESS_STAKING = upStaking.address
+  console.log("staking deployed to:", ADDRESS_STAKING);
+
+  //SetMinter on ERC20
+  const setMinterTx = await erc20.setMinterAddress(ADDRESS_STAKING);
+  await setMinterTx.wait();
+
+  console.log("Should be true:", await erc20.getMinter(ADDRESS_STAKING))
+
 }
 
-deployStakingUpgradeable().catch((error) => {
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
