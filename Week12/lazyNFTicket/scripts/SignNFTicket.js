8b488c30b4598d8d73b21184a38e1e3db1a39395
your_username <stztrujillo@hotmail.com>
2023-04-10 16:10:20 +0100

    tried different tokenId and prices

--- Week12/lazyNFTicket/scripts/SignNFTicket.js
+++ Week12/lazyNFTicket/scripts/SignNFTicket.js
@@ -6,12 +6,12 @@ const { ethers } = require("ethers");
 //4. Generate the signature
 
 //~~~~~~ Constants to generate the signature ~~~~~~
-const SIGNER_GANACHE = new ethers.Wallet("50c20848802ade8a7c87c364316b069a71b4290643d731d28eb27012184cd162");
+const SIGNER_GANACHE = new ethers.Wallet("754dfe3c78f7ad3023bc78ecbdb19a5a045b4a441414f36f76139a29037d3806");
 const DOMAIN_NAME = "Vouchers-Santiago"; //Same as the smart contract
 const VERSION = "1"; //Same as the smart contract
 
 const CHAIN_ID = "1";
-const CONTRACT_ADDRESS = "0xB6e677a576B56581B027B20E1F2aBea290906a93";
+const CONTRACT_ADDRESS = "0x584C10E01bC1d4BE2993c6a1Cc2f6E7Bdb7A3F72";
 //Create domain, types and ticket
 async function createNFTicket(tokenId, buyer, price, uri) {
   const domain = {
@@ -39,7 +39,7 @@ async function createNFTicket(tokenId, buyer, price, uri) {
 
 //Call createNFTicket() from this function
 async function sellTicket() {
-  const tokenId = "1";
+  const tokenId = "2";
   const buyer = "0xa5676d9158213e84504d489764e6b85fEeac5Ea8";
   const price = "1000000000000000000";
   const uri = `ipfs://QmQ82jo2ymatk9DCyNKENGo6ULYYRUtryRdtsZ1YgYqUZm/${tokenId}`;
