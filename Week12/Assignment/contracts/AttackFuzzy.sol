f0bf9d1e5f0e469181db56fc43e7fcc0a9201fa8
your_username <stztrujillo@hotmail.com>
2023-04-14 18:36:17 +0100

    solved FuzzyIdentity

--- Week12/Assignment/contracts/AttackFuzzy.sol
+++ Week12/Assignment/contracts/AttackFuzzy.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.21;
 
+//1. require(isSmarx(msg.sender)) will pass by calling the contract from Attack contract
+//2. require(isBadCode(msg.sender)); to pass this, we need to pass a wallet that contains 
+//      the hex code "badc0de" 
+//      this can be done by brute force by creating wallets and creating contracts with those wallets
+//      until the bytes20(keccak256(addressUser, nonce)) can be equal to "badc0de"
+
 interface IName {
     function name() external view returns (bytes32);
 }
