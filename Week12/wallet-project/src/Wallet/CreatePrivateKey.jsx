b8d0c233528a3b049dda6b7c7e67802ac281e9e7
your_username <stztrujillo@hotmail.com>
2023-04-17 09:27:28 +0100

    create algorythm to generate a private key

--- Week12/wallet-project/src/Wallet/CreatePrivateKey.jsx
+++ Week12/wallet-project/src/Wallet/CreatePrivateKey.jsx
@@ -1,24 +1,26 @@
+import * as ethers from "ethers";
 import * as bip39 from "@scure/bip39";
 import { wordlist } from "@scure/bip39/wordlists/english";
 
-export async function main() {
+export function createPk() {
   // Generate x random words. Uses Cryptographically-Secure Random Number Generator.
-  const mn = bip39.generateMnemonic(wordlist);
-  console.log(mn);
-
-  // Reversible: Converts mnemonic string to raw entropy in form of byte array.
-  const ent = bip39.mnemonicToEntropy(mn, wordlist);
-  console.log(ent);
-
-  // Reversible: Converts raw entropy in form of byte array to mnemonic string.
-  const check1 = bip39.entropyToMnemonic(ent, wordlist);
-  console.log(check1);
-  // Validates mnemonic for being 12-24 words contained in `wordlist`.
-  const check2 = bip39.validateMnemonic(mn, wordlist);
-  console.log(check2);
+  const mnemonic = bip39.generateMnemonic(wordlist);
 
   // Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
-  await bip39.mnemonicToSeed(mn, "password");
-  bip39.mnemonicToSeedSync(mn, "password");
+  //derive seed from the mnemonic (uint8Array of 64)
+  const seedUint8 = bip39.mnemonicToSeedSync(mnemonic);
+
+  const seedHex = ethers.utils.hexlify(seedUint8);
+
+  // Validates mnemonic for being 12-24 words contained in `wordlist`.
+  //const verifyMnemonic = bip39.validateMnemonic(mnemonic, wordlist);
+
+  return { MNEMONIC: mnemonic, PRIVATE_KEY_UINT8: seedUint8, PRIVATE_KEY_HEX: seedHex };
+
+  //~~~~~~~~~~~~~~ 2ND OPTION ~~~~~~~~~~~~~~
+  //instead of using bip39 library, I can generate an Uint8Array(64) of random numbers from 0 to 255
+  //then convert this array to hex
+  //but in this case there won't be an "mnemonic"
 }
-main();
+
+export const createPK = createPk;
