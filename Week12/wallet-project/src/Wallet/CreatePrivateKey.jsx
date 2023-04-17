975baa5627ba19a56ac39a46c2ed58fa37ea875e
your_username <stztrujillo@hotmail.com>
2023-04-17 10:58:36 +0100

    separated the function in 3 functions

--- Week12/wallet-project/src/Wallet/CreatePrivateKey.jsx
+++ Week12/wallet-project/src/Wallet/CreatePrivateKey.jsx
@@ -6,16 +6,10 @@ export function createPk() {
   // Generate x random words. Uses Cryptographically-Secure Random Number Generator.
   const mnemonic = bip39.generateMnemonic(wordlist);
 
-  // Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
-  //derive seed from the mnemonic (uint8Array of 64)
-  const seedUint8 = bip39.mnemonicToSeedSync(mnemonic);
-
-  const seedHex = ethers.utils.hexlify(seedUint8);
-
   // Validates mnemonic for being 12-24 words contained in `wordlist`.
   //const verifyMnemonic = bip39.validateMnemonic(mnemonic, wordlist);
 
-  return { MNEMONIC: mnemonic, PRIVATE_KEY_UINT8: seedUint8, PRIVATE_KEY_HEX: seedHex };
+  return mnemonic;
 
   //~~~~~~~~~~~~~~ 2ND OPTION ~~~~~~~~~~~~~~
   //instead of using bip39 library, I can generate an Uint8Array(64) of random numbers from 0 to 255
@@ -23,4 +17,14 @@ export function createPk() {
   //but in this case there won't be an "mnemonic"
 }
 
-export const createPK = createPk;
+export function mnemonicToUint8(mnemonic) {
+  // Irreversible: Uses KDF to derive 64 bytes of key data from mnemonic + optional password.
+  //derive seed from the mnemonic (uint8Array of 64)
+  const seedUint8 = bip39.mnemonicToSeedSync(mnemonic);
+  return seedUint8;
+}
+
+export function uint8ToHex(arrayUint8) {
+  const seedHex = ethers.utils.hexlify(arrayUint8);
+  return seedHex;
+}
