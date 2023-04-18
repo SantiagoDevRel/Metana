4cbdcaa199ddf6c2fa4b2bc8c72bf771ccbb7916
your_username <stztrujillo@hotmail.com>
2023-04-18 12:23:27 +0100

    updated footer to load the current account and nonce

--- Week12/wallet-project/src/Components/Footer.jsx
+++ Week12/wallet-project/src/Components/Footer.jsx
@@ -1,20 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Footer.module.css";
 function Footer(props) {
-  const [currentNonce, setCurrentNonce] = useState("0x");
-  const [currentAddress, setCurrentAddress] = useState("0x");
-  console.log("FOOTER", props.mainWallet);
-
-  useEffect(() => {
-    const { currentAddress, currentNonce } = props.mainWallet;
-    setCurrentNonce(currentNonce);
-    setCurrentAddress(currentAddress);
-    console.log("currentadd", currentAddress);
-  });
   return (
     <div className={styles.footer}>
-      <div>Current account: {currentAddress} </div>
-      <div>Nonce: {currentNonce}</div>
+      <div>Current account: {props.address} </div>
+      <div>Nonce: {props.nonce}</div>
     </div>
   );
 }
