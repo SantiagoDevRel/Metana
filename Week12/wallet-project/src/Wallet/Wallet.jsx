db87b9d97df13417e23aad08b3b02c6080e0f17f
your_username <stztrujillo@hotmail.com>
2023-04-18 08:45:23 +0100

    added hdnode functionalities

--- Week12/wallet-project/src/Wallet/Wallet.jsx
+++ Week12/wallet-project/src/Wallet/Wallet.jsx
@@ -2,29 +2,95 @@ import * as ethers from "ethers";
 import { createPk } from "./CreatePrivateKey";
 
 export class Wallet {
-  chainId;
-  provider;
-  nonce;
   mnemonic;
-  privateKeyUint8;
-  privateKeyHex;
+  seedUint8;
+  seedHex;
+  chainId;
+  nonce;
+  address;
+  currentAccount;
+  currentPrivateKeyToSign;
 
-  accounts = [];
+  privateKeysEVM = [];
+  accountsEVM = [];
   assets = [];
   activity = [];
 
   constructor(_mnemonic, _privateKeyUint8, _privateKeyHex) {
     this.nonce = 0;
     this.mnemonic = _mnemonic;
-    this.privateKeyUint8 = _privateKeyUint8;
-    this.privateKeyHex = _privateKeyHex;
-
+    this.seedUint8 = _privateKeyUint8;
+    this.seedHex = _privateKeyHex;
     this.chainId = 80001; //Only compatible with Mumbai for now
+    this.setupWallet();
+  }
+
+  get nonce() {
+    return this.nonce;
+  }
+
+  get privateKeysEVM() {
+    return this.privateKeysEVM;
+  }
+
+  get mnemonic() {
+    return this.mnemonic;
+  }
+
+  get address() {
+    return this.address;
+  }
+
+  get assets() {
+    return this.assets;
+  }
+
+  get activity() {
+    return this.activity;
+  }
+
+  get currentAccount() {
+    return this.currentAccount;
   }
 
   signTransaction() {}
 
-  createNewAccount() {}
+  setupWallet() {
+    //instanciate the  Hierarchical Deterministic Node
+    const HD_NODE = ethers.utils.HDNode.fromMnemonic(this.mnemonic);
+    //setup first private
+    const firstPrivateKey = HD_NODE.privateKey;
+    this.privateKeysEVM.push(firstPrivateKey);
+    //setup first address(account)
+    const firstAccount = HD_NODE.address;
+    this.accountsEVM.push(firstAccount);
 
-  changeAccount() {}
+    //Account and private key MUST have the same index
+    this.currentAccount = this.accountsEVM[0];
+    this.currentPrivateKeyToSign = this.privateKeyEVM[0];
+  }
+
+  createNewAccount() {
+    //get mnemonic
+    const _mnemomic = this.mnemonic;
+    //get default ethereum path
+    const _path = ethers.utils.defaultPath;
+    //get the current HDNODE
+    const HD_NODE = ethers.utils.HDNode.fromMnemonic(_mnemomic);
+    //derive a path to get a child node from our HDNode
+    const childNode = HD_NODE.derivePath(_path);
+
+    //push new private key
+    const privateKey = childNode.privateKey;
+    this.privateKeysEVM.push(privateKey);
+
+    //push new account
+    const address = ethers.utils.computeAddress(privateKey);
+    this.accountsEVM.push(address);
+  }
+
+  changeAccount(index) {
+    this.currentAccount = this.accountsEVM[index];
+    this.currentPrivateKeyToSign = this.privateKeysEVM[index];
+  }
 }
