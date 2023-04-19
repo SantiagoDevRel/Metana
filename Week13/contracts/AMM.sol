acb1a7bd2b2c7c3e5f86f940c4b15af398e91b27
your_username <stztrujillo@hotmail.com>
2023-04-19 12:12:03 +0100

    implemented remove liquidity

--- Week13/contracts/AMM.sol
+++ Week13/contracts/AMM.sol
@@ -36,15 +36,15 @@ contract AMM{
     function swap(IERC20 _tokenIn, uint256 _amountIn) external returns(uint256 amountOut) {
         require(_tokenIn == tokenA || _tokenIn == tokenB, "AMM: Invalid token");
         address _user = msg.sender;
-        address thisContract = address(this);
+        address _thisContract = address(this);
         //1. transfer tokenIn to address(this)
 
         uint256 amountIn;
         if(_tokenIn == tokenA){
             //1. transfer tokenIn to address(this)
-            tokenA.transferFrom(_user,thisContract,_amountIn);
+            tokenA.transferFrom(_user,_thisContract,_amountIn);
             //recalculate amountIn for security reasons --> balanceOf - against the reserves
-            amountIn = tokenA.balanceOf(thisContract) - reserveTokenA;
+            amountIn = tokenA.balanceOf(_thisContract) - reserveTokenA;
 
             //2. calculate the fee on tokenIn
             uint256 feeTokenIn = _calculateFee(amountIn);
@@ -53,17 +53,17 @@ contract AMM{
             amountOut = amountIn - feeTokenIn;
 
             //4. update the reserve token A and B
-            _addReserveA(amountIn);
-            _removeReserveB(amountOut);
+            _increaseReserveA(amountIn);
+            _decreaseReserveB(amountOut);
 
             //5. transfer amountOut to the user
             require(tokenB.transfer(_user,amountOut),"AMM: Error transfering the funds to the user");
 
         }else{
             //1. transfer tokenIn to address(this)
-            tokenB.transferFrom(_user,thisContract,_amountIn);
+            tokenB.transferFrom(_user,_thisContract,_amountIn);
             //recalculate amountIn for security reasons --> balanceOf - against the reserves
-            amountIn = tokenB.balanceOf(thisContract) - reserveTokenA;
+            amountIn = tokenB.balanceOf(_thisContract) - reserveTokenA;
 
             //2. calculate the fee on tokenIn
             uint256 feeTokenIn = _calculateFee(amountIn);
@@ -72,23 +72,70 @@ contract AMM{
             amountOut = amountIn - feeTokenIn;
 
             //4. update the reserve token A and B
-            _addReserveB(amountIn);
-            _removeReserveA(amountOut);
+            _increaseReserveB(amountIn);
+            _decreaseReserveA(amountOut);
 
             //5. transfer amountOut to the user
             require(tokenA.transfer(_user,amountOut),"AMM: Error transfering the funds to the user");
         }
     }
 
-    function addLiquidity() external {
+    function addLiquidity(uint256 _amountTokenA, uint256 _amountTokenB) external returns(uint256 sharesToMint) {
+        //transfer tokens from user to thisContract
+        address _thisContract = address(this);
+        address _user = msg.sender;
+        require(tokenA.transferFrom(_user, _thisContract, _amountTokenA),"AMM: Failed transfering token A");
+        require(tokenB.transferFrom(_user,_thisContract, _amountTokenB),"AMM: Failed transfering token B");
 
+        //recalculate the amounts in for security reasons
+        uint256 _amountA = tokenA.balanceOf(_thisContract) - reserveTokenA;
+        uint256 _amountB = tokenB.balanceOf(_thisContract) - reserveTokenB;
+
+        //calculate shares
+        sharesToMint = _calculateSharesToMint(_amountA, _amountB);
+        require(sharesToMint>0, "AMM: Shares can't be 0");
+        _mintShares(_user, sharesToMint);
+
+        //update the reserves
+        _increaseReserveA(_amountA);
+        _increaseReserveB(_amountB);
     }
 
-    function removeLiquidity() external {
+    function removeLiquidity(uint256 _sharesToBurn) external returns(uint256 tokenAOut, uint256 tokenBOut){
+        address _user = msg.sender;
 
+        //burn the shares
+        _burnShares(_user, _sharesToBurn);
+
+        //calculate the amount of tokens to give to the user
+        (tokenAOut, tokenBOut) = _calculateAmountsOut(_sharesToBurn);
+
+        //decrease both reserves with the token amounts
+        _decreaseReserveA(tokenAOut);
+        _decreaseReserveB(tokenBOut);
+
+        //transfer both tokens back to the user
+        if(tokenAOut > 0){
+            tokenA.transfer(_user, tokenAOut);
+        }
+        if(tokenBOut > 0){
+            tokenB.transfer(_user, tokenBOut);
+        }
     }
 
     // ~~~~~~~ Internal/Private functions ~~~~~~~ 
+    function _calculateSharesToMint(uint256 _amountA, uint256 _amountB) private view returns(uint256){
+        if(getTotalShares() > 0){
+            return ((_amountA + _amountB) * getTotalShares()) / (getReserveA() - getReserveB());
+        }
+        return (_amountA + _amountB);
+    }
+
+    function _calculateAmountsOut(uint256 _sharesToBurn) private view returns(uint256 _tokenAOut, uint256 _tokenBOut){
+        _tokenAOut = (getReserveA()*_sharesToBurn) / getTotalShares();
+        _tokenBOut = (getReserveB() * _sharesToBurn) / getTotalShares();
+    }
+    
     function _mintShares(address _to, uint256 _amount) internal{
         sharesPerUser[_to] += _amount;
         totalShares += _amount;
@@ -100,24 +147,37 @@ contract AMM{
 
     }
 
-    function _addReserveA(uint256 _amountIn) private {
+    function _increaseReserveA(uint256 _amountIn) private {
         reserveTokenA += _amountIn;
     }
 
-    function _removeReserveA(uint256 _amountOut) private {
+    function _decreaseReserveA(uint256 _amountOut) private {
         reserveTokenA -= _amountOut;
     }
 
-    function _addReserveB(uint256 _amountIn) private {
+    function _increaseReserveB(uint256 _amountIn) private {
         reserveTokenB += _amountIn;
     }
 
-    function _removeReserveB(uint256 _amountOut) private {
+    function _decreaseReserveB(uint256 _amountOut) private {
         reserveTokenB -= _amountOut;
     }
 
     function _calculateFee(uint256 amount) private view returns(uint256){
         return (amount * tradingFeeBase1000) / BASE_PERCENTAGE;
+    }
+
+    // ~~~~~~~ Pure/View functions ~~~~~~~ 
+    function getReserveA() public view returns(uint256){
+        return reserveTokenA;
+    }
+    
+    function getReserveB() public view returns(uint256){
+        return reserveTokenB;
+    }
+
+    function getTotalShares() public view returns(uint256){
+        return totalShares;
     }
 
 }
