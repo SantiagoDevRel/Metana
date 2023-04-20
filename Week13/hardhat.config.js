f4a20db8d8c33300d9714f9f440271be3ac93436
your_username <stztrujillo@hotmail.com>
2023-04-20 11:27:27 +0100

    add upgradeable plugin

--- Week13/hardhat.config.js
+++ Week13/hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+
 require("dotenv").config();
 
 const { PRIVATE_KEY, MUMBAI_NODE, POLY_KEY } = process.env;
