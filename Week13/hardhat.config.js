b1f28757943b03cf72cc9932e267c1f6d5d2e610
your_username <stztrujillo@hotmail.com>
2023-04-20 10:06:49 +0100

    updated hardhat config with mumbai

--- Week13/hardhat.config.js
+++ Week13/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { PRIVATE_KEY, MUMBAI_NODE, POLY_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: MUMBAI_NODE,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: POLY_KEY,
+  },
 };
