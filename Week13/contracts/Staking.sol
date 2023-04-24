d53521283b788a7807d0846ca3ed888ebbc6330d
your_username <stztrujillo@hotmail.com>
2023-04-24 07:37:42 +0100

    created state variables

---
+++ Week13/contracts/Staking.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Staking is Ownable{
+
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardsToken;
+
+    uint256 private duration;
+    uint256 private finishAt;
+    uint256 private updatedAt;
+    uint256 private rewardPerSecond;
+    uint256 private rewardPerTokenStored;
+    mapping (address => uint256) private userRewardPerTokenPaid;
+    mapping (address => uint256) private rewardsEarned;
+
+    uint256 private totalStaked;
+    mapping (address => uint256) private balanceOf;
+
+    constructor(address _staking, address _rewards){
+        stakingToken = IERC20(_staking);
+        rewardsToken = IERC20(_rewards);
+    }
+
+    
+
+
+}
