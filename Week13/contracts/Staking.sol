0210ca8359c6016a6df3734abfdcf62b2c2f16cc
your_username <stztrujillo@hotmail.com>
2023-04-24 08:25:41 +0100

    implemented functions to set the duration of staking and rewardsRate

--- Week13/contracts/Staking.sol
+++ Week13/contracts/Staking.sol
@@ -13,15 +13,15 @@ contract Staking is Ownable{
     IERC20 public immutable rewardsToken;
 
     //~~~~~~ State variables ~~~~~~
-    uint256 private duration;
-    uint256 private finishAt;
-    uint256 private updatedAt;
-    uint256 private rewardPerSecond;
-    uint256 private rewardPerTokenStored;
+    uint256 private duration; //duration while rewards are being given
+    uint256 private finishAt; //time where the staking is over
+    uint256 private updatedAt; //last time update the rewards
+    uint256 private rewardPerSecond; //rewards that will be given in total to the users per second
+    uint256 private rewardPerTokenStored; //rate reward per second(depending on totalStaked)
     mapping (address => uint256) private userRewardPerTokenPaid;
     mapping (address => uint256) private rewardsEarned;
 
-    uint256 private totalStaked;
+    uint256 private totalStaked;    
     mapping (address => uint256) private balanceOf;
 
     //~~~~~~ Constructor ~~~~~~
@@ -33,14 +33,39 @@ contract Staking is Ownable{
     //~~~~~~ Public/External functions ~~~~~~
 
     //set the duration for this staking contract
-    function setRewardsDuration(uint256 _duration) external onlyOwner {
-        require(block.timestamp > finishAt, "Staking: Wait until the round is over");
+    function setDuration(uint256 _duration) external onlyOwner {
+        require(duration == 0, "Staking: duration already set");
         duration = _duration;
     }
 
-    //set the total amount of rewards that will be paid for this specific above duration
-    function notifyRewardAmount(uint256 _amount) external onlyOwner{
+    //set the total amount of rewards that will be paid for the specific duration
+    //add initial amount of rewards to start the contract
+    function addRewardsToStart(address _from, uint256 _amount) external onlyOwner{
+        require(duration > 0, "Staking: duration hasn't been set yet");
+        require(rewardPerSecond == 0, "Staking: rewards rate already set");
+        require(rewardsToken.transferFrom(_from, address(this), _amount));
+        rewardPerSecond = _amount / duration;
+        require(_validateRewardsAndTime(rewardPerSecond, duration));
+        //emit RewardsAdded()
+    }
 
+    //add more rewards to the pool of rewards while the contract is running
+    function addRewardsWhileStaking(address _from, uint256 _amount) external onlyOwner{
+        require(rewardPerSecond > 0, "Staking: rewards rate hasn't been set yet");
+        require(rewardsToken.transferFrom(_from, address(this), _amount));
+        uint256 remainingRewards = rewardPerSecond * (finishAt - block.timestamp);
+        rewardPerSecond = (remainingRewards + _amount) / duration;
+        require(_validateRewardsAndTime(rewardPerSecond, duration));
+        //emit RewardsAdded()
+    }
+
+    function _validateRewardsAndTime(uint256 _rewardPerSecond, uint256 _duration) internal returns(bool){
+        require(_rewardPerSecond > 0, "Staking: reward rate can't be 0");
+        require(rewardsToken.balanceOf(address(this)) >= _rewardPerSecond * _duration,"Staking: Insufficient funds to pay rewards");
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+        //emit FinishTimeSet()
+        return true;
     }
 
     function stake(uint256 _amount) external{
