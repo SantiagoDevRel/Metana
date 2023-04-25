b06523fba17f872e798ed78f8286562268488cec
your_username <stztrujillo@hotmail.com>
2023-04-25 12:50:47 +0100

    created testuniswap

---
+++ Week13/Uniswap/contracts/TestUniswap.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IERC20.sol";
+
+contract Test{
+
+
+
+}
