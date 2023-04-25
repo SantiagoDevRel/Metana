fb05ee002c500cf96c66a9855b02750514d029e9
your_username <stztrujillo@hotmail.com>
2023-04-25 12:57:27 +0100

    added uniswap v2 periphery

--- Week13/Uniswap/contracts/TestUniswap.sol
+++ Week13/Uniswap/contracts/TestUniswap.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/v2-core/contracts/interfaces/IERC20.sol";
 
 contract Test{
