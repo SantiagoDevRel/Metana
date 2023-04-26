2f3ddf08274c5d74a887f61f3b7a9064770d34c6
your_username <stztrujillo@hotmail.com>
2023-04-26 12:13:38 +0100

    add function add liquidity

--- Week13/Uniswap/contracts/AddLiquidity.sol
+++ Week13/Uniswap/contracts/AddLiquidity.sol
@@ -4,8 +4,16 @@ pragma solidity 0.8.18;
 import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-contract AddLq{
+contract AddLiquidity{
+
+    address constant public UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    function addLiquidity(address tokenA, address tokenB) external {
+
+        
+    }
 
 
-    
+
+
 }
