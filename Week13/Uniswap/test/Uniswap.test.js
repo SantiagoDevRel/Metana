e70efd90cfe570d70344defd088f60d6bb11eb6c
your_username <stztrujillo@hotmail.com>
2023-04-26 08:24:40 +0100

    test dai balance

--- Week13/Uniswap/test/Uniswap.test.js
+++ Week13/Uniswap/test/Uniswap.test.js
@@ -1,23 +1,38 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { network, ethers } = require("hardhat");
+const { BigNumber } = ethers;
 
-describe("Lock", function () {
+//address on mainnet
+const WHITEBIT_ADDRESS = "0x1689a089AA12d6CbBd88bC2755E4c192f8702000";
+const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+describe("Swap", function () {
+  let dai, whitebitSigner;
   async function testSwapUniswap() {
-    const [owner] = await ethers.getSigners();
-    const WHITEBIT_ADDRESS = "0x1689a089AA12d6CbBd88bC2755E4c192f8702000";
-    const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    //impersonate account parameters
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [WHITEBIT_ADDRESS],
+    });
+    //instance of signer
+    whitebitSigner = await ethers.getSigner(WHITEBIT_ADDRESS);
 
-    const Swap = await ethers.getContractFactory("Lock");
+    //get instace of DAI
+    dai = await ethers.getContractAt("IERC20", DAI_ADDRESS);
+    console.log("BAALNCE", await dai.balanceOf(whitebitSigner.address));
+
+    const Swap = await ethers.getContractFactory("Test");
     const swapContract = await Swap.deploy();
 
-    return { swapContract, owner, BINANCE: WHITEBIT_ADDRESS, DAI_ADDRESS };
+    return { swapContract, whitebitSigner, dai };
   }
 
   describe("Deployment", function () {
     it("Should set the right unlockTime", async function () {
-      const { swapContract, owner, BINANCE: WHITEBIT_ADDRESS, DAI_ADDRESS } = await loadFixture(testSwapUniswap);
+      const { swapContract, whitebitSigner, dai } = await loadFixture(testSwapUniswap);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      console.log();
     });
   });
 });
