4d76d5ba24758d7e1397a35b766bf54b019accd4
your_username <stztrujillo@hotmail.com>
2023-04-27 14:12:25 +0100

    fixed name of the contract usdcContract

--- Week13/Uniswap/test/AddLiquidity.test.js
+++ Week13/Uniswap/test/AddLiquidity.test.js
@@ -8,7 +8,7 @@ const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
 describe("Swap", function () {
-  let daiContract, whitebitSigner, wbtcContract;
+  let daiContract, whitebitSigner, usdcContract;
   async function testSwapUniswap() {
     //impersonate account parameters
     await network.provider.request({
