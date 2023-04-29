e0cbd767a72b943e9a14473ae841bba91f9efea0
your_username <stztrujillo@hotmail.com>
2023-04-29 09:44:33 +0100

    created flashswap contract

---
+++ Week13/Uniswap/contracts/FlashSwap.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+contract FlashSwap {
+
+
+}
