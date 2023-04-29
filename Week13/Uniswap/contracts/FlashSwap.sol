58bb9cd27e84c1fa8da3b46ebe1f7a469a845ae5
your_username <stztrujillo@hotmail.com>
2023-04-29 10:09:18 +0100

    hardcoded the weth, uniswap router & factory contracts on mainnet

--- Week13/Uniswap/contracts/FlashSwap.sol
+++ Week13/Uniswap/contracts/FlashSwap.sol
@@ -9,5 +9,13 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract FlashSwap {
 
+    address constant public UNISWAP_ROUTER_V2 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address constant public WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant public UNISWAP_FACTORY_V2 = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    
+    function flashSwap(address _tokenToBorrow, uint256 _amount) external{
+        
+
+    }
 
 }
