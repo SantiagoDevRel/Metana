8823b63efe409ee8d4cef2313083fbc4bfc34fa7
your_username <stztrujillo@hotmail.com>
2023-04-30 08:18:20 +0100

    added the key with dotenv

--- Week5/eth-client-assignment/src/ERC20Logs.js
+++ Week5/eth-client-assignment/src/ERC20Logs.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Network, Alchemy } from "alchemy-sdk";
 import { ethers } from "ethers";
 import styles from "./ERC20Logs.module.css";
+const KEY = process.env.REACT_APP_KEY;
+
 const ALCHEMY_URL = `https://eth-mainnet.g.alchemy.com/v2/`;
-const ALCHEMY_KEY = `uk3L_f3i_POcVTRhWxKrYlUm__ftnzfm`;
+const ALCHEMY_KEY = KEY;
 
 const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-const TransferSignature =
-  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-const ApproveSignature =
-  "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
+const TransferSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+const ApproveSignature = "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
 
 function ERC20Logs() {
   useEffect(() => {
@@ -103,19 +103,13 @@ function ERC20Logs() {
   return (
     <>
       <div className={styles.containerButtons}>
-        <button
-          className={styles.button}
-          onClick={() => formatLogs(`${TransferSignature}`)}
-        >
+        <button className={styles.button} onClick={() => formatLogs(`${TransferSignature}`)}>
           Update Transfer Logs
         </button>
         <div className={styles.title}>
           From the block #{startingBlock} <br></br> to the block #{currentBlock}
         </div>
-        <button
-          className={styles.button}
-          onClick={() => formatLogs(`${ApproveSignature}`)}
-        >
+        <button className={styles.button} onClick={() => formatLogs(`${ApproveSignature}`)}>
           Update Approval Logs
         </button>
       </div>
@@ -127,18 +121,8 @@ function ERC20Logs() {
                 <div>From: {tx.from}</div>
                 <div>To: {tx.to}</div>
                 <div>
-                  <a
-                    className={styles.a}
-                    href={tx.txHash}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View in Etherscan{" "}
-                    <img
-                      className={styles.img}
-                      alt="Etherscan Logo"
-                      src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg"
-                    />
+                  <a className={styles.a} href={tx.txHash} target="_blank" rel="noreferrer">
+                    View in Etherscan <img className={styles.img} alt="Etherscan Logo" src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg" />
                   </a>
                 </div>
               </li>
@@ -154,18 +138,8 @@ function ERC20Logs() {
                 <div>Approval From: {tx.from}</div>
                 <div>Spender: {tx.to}</div>
                 <div>
-                  <a
-                    className={styles.a}
-                    href={tx.txHash}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View in Etherscan{" "}
-                    <img
-                      className={styles.img}
-                      alt="Etherscan Logo"
-                      src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg"
-                    />
+                  <a className={styles.a} href={tx.txHash} target="_blank" rel="noreferrer">
+                    View in Etherscan <img className={styles.img} alt="Etherscan Logo" src="https://etherscan.io/images/brandassets/etherscan-logo-circle-light.svg" />
                   </a>
                 </div>
               </li>
