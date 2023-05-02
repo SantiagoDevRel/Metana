faf0cbe50a9c6dfa1e933b16a757e267ff993351
your_username <stztrujillo@hotmail.com>
2023-05-02 07:56:36 +0100

    created flashmint contract

---
+++ Week13/Uniswap/contracts/FlasMint.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
+
+contract FlashMint {
+
+    address public constant IWETH10 = 0xf4BB2e28688e89fCcE3c0580D37d36A7672E8A9F;
+    
+
+
+}
