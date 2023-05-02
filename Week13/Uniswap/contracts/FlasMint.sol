c859e024ba8976763b226e73b9f05246c62c1a83
your_username <stztrujillo@hotmail.com>
2023-05-02 09:58:06 +0100

    added approve token

--- Week13/Uniswap/contracts/FlasMint.sol
+++ Week13/Uniswap/contracts/FlasMint.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 
-contract FlashMint is IERC3156FlashBorrower {
+contract FlashMint {
 
-    address public constant IWETH10 = 0xf4BB2e28688e89fCcE3c0580D37d36A7672E8A9F;
+    address public constant WETH10 = 0xf4BB2e28688e89fCcE3c0580D37d36A7672E8A9F;
     bytes32 public immutable CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
     
     address public s_initiator;
@@ -19,13 +19,14 @@ contract FlashMint is IERC3156FlashBorrower {
     bytes public s_bytes;
 
     function flash() external{
-        uint256 totalSupplyToken = IERC20(IWETH10).totalSupply();
-        s_iwethTotalSupplyBeforeFlashLoan = totalSupplyToken;
+        s_iwethTotalSupplyBeforeFlashLoan = IERC20(WETH10).totalSupply();
 
-        uint256 amount = totalSupplyToken + 1;
+        uint256 amount = s_iwethTotalSupplyBeforeFlashLoan + 1;
         bytes memory data = abi.encode(msg.sender);
 
-        IERC3156FlashLender(IWETH10).flashLoan(IERC3156FlashBorrower(address(this)), IWETH10, amount, data);
+        IERC20(WETH10).approve(WETH10, amount);
+
+        IERC3156FlashLender(WETH10).flashLoan(IERC3156FlashBorrower(address(this)), WETH10, amount, data);
     }
 
     function onFlashLoan(
@@ -37,7 +38,7 @@ contract FlashMint is IERC3156FlashBorrower {
     ) external returns (bytes32){
         //here I should do whatever I want with the IWETH I just borrowed
         s_initiator = initiator;
-        s_iwethTotalSupplyDuringFlashLoan = IERC20(IWETH10).totalSupply();
+        s_iwethTotalSupplyDuringFlashLoan = IERC20(WETH10).totalSupply();
         s_token = token;
         s_balance = amount;
         s_fee = fee;
